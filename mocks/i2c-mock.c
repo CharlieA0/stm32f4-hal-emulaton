751ba7a9c37b25298531a2f76e05aaafcd7d919f
Charlie <CharlieA0@users.noreply.github.com>
2019-01-21 14:20:44 -0500

    Removed debugging statements

--- mocks/i2c-mock.c
+++ mocks/i2c-mock.c
@@ -17,13 +17,9 @@ uint16_t receive_address;
 // Mocks the functionality of i2c transmit by writing each value to tx_queue
 // Always sets transmit_address to DevAddress before writing
 HAL_StatusTypeDef HAL_I2C_Master_Transmit(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint8_t *pData, uint16_t Size, uint32_t Timeout) {
-
-  //printf(" A  %#x   ", DevAddress);
   
   transmit_address = DevAddress;
     
-  //printf(" B  %#x   ", transmit_address);
-
   TEST_ASSERT_NOT_NULL_MESSAGE(tx_queue_start, "I2C Mock Failed: tx_queue's start wasn't initialized");
   TEST_ASSERT_NOT_NULL_MESSAGE(tx_queue_end, "I2C Mock Failed: tx_queue'end wasn't initialized");
   TEST_ASSERT_TRUE_MESSAGE(tx_queue_start < tx_queue_end, "I2C Mock Failed: tx_queue's start is after its end");
@@ -33,8 +29,6 @@ HAL_StatusTypeDef HAL_I2C_Master_Transmit(I2C_HandleTypeDef *hi2c, uint16_t DevA
     *(tx_queue_start) = *(pData + i);
     tx_queue_start++;
   }
-
-  //printf(" C  %#x   ", transmit_address);
 
   return HAL_OK;
 
